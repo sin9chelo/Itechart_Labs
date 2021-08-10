5ad5d9ead75b80208f73c1639050e0c5ca99c1a4
sin9chelo <sinkevichh.kirill@gmail.com>
2021-08-10 19:03:51 +0300

    added persisting the login, async pipe, home page and register form

--- WebApp/client/src/app/nav/nav.component.ts
+++ WebApp/client/src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,13 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-  constructor() { }
+  loggedIn: boolean = false;
+
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
   login() {
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+    }, error =>{
+      console.log(error);      
+    })
   }
 
+  logout() {
+    this.accountService.logout();
+  }
 }
