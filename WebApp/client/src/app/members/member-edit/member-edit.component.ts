d706d8cbc5e128cf35c1f29aa68bb04a2979ef41
sin9chelo <sinkevichh.kirill@gmail.com>
2021-08-18 19:55:20 +0300

    updated the user in the client app, added loading, used service to store data

--- WebApp/client/src/app/members/member-edit/member-edit.component.ts
+++ WebApp/client/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs/operators';
@@ -16,6 +16,11 @@ export class MemberEditComponent implements OnInit {
   @ViewChild('editForm') editForm!: NgForm;
   member!: Member;
   user!: User;
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any) {
+    if(this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
 
   constructor(private accountService: AccountService, private memberService: MembersService,
     private toastr: ToastrService) {
@@ -33,8 +38,9 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateMember() {
-    console.log(this.member);
-    this.toastr.success('Profile updated successfully');
-    this.editForm.reset(this.member);
+    this.memberService.updateMember(this.member).subscribe(() => {
+      this.toastr.success('Profile updated successfully');
+      this.editForm.reset(this.member); 
+    })
   }
 }
