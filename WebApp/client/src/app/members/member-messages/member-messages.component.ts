3076b044c3d55ca461477c16451002692d8d30e3
sin9chelo <sinkevichh.kirill@gmail.com>
2021-08-26 20:38:25 +0300

    activate message tab and use query params

--- WebApp/client/src/app/members/member-messages/member-messages.component.ts
+++ WebApp/client/src/app/members/member-messages/member-messages.component.ts
@@ -8,18 +8,13 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit {
+  @Input() messages!: Message[];
   @Input() username!: string;
-  messages!: Message[];
 
   constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
-    this.loadMessages();
   }
 
-  loadMessages() {
-    this.messageService.getMessageThread(this.username).subscribe(messages =>{
-      this.messages = messages;
-    })
-  }
+
 }
