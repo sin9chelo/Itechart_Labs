4f18fe4fc9a52ff54808f5f1ee13ea184dd9d4d2
sin9chelo <sinkevichh.kirill@gmail.com>
2021-08-31 01:18:21 +0300

    add the hub connection to the message service and sending messages via the hub

--- WebApp/API/Startup.cs
+++ WebApp/API/Startup.cs
@@ -74,7 +74,7 @@ namespace API
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
-                endpoints.MapHub<MessageHub>("hubs/presence");
+                endpoints.MapHub<MessageHub>("hubs/message");
             });
         }
     }
