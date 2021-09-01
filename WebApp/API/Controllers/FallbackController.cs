b0a2994da21a630df786bc997af28011f0246a36
sin9chelo <sinkevichh.kirill@gmail.com>
2021-09-01 09:13:19 +0300

    pre switch to postgres

--- WebApp/API/Controllers/FallbackController.cs
+++ WebApp/API/Controllers/FallbackController.cs
@@ -11,7 +11,8 @@ namespace API.Controllers
     {
         public ActionResult Index()
         {
-            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/HTML");
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
+                "wwwroot", "index.html"), "text/HTML");
         }
     }
 }
